cbb2cb01f7c9708ad8b808633c0b04ad9bbfab89
Guido Arkesteijn <guiddarkesteijn@gmail.com>
2021-01-01 11:03:25 +0100

    Started work on a simple LifetimeSystem
    This is still missing the remove childs of a parent. (if needed?)

---
+++ Assets/Scripts/Lifetime/CleanupEntitiesSystem.cs
@@ -0,0 +1,29 @@
+﻿using Unity.Entities;
+
+[UpdateAfter(typeof(LifetimeSystem))]
+public class CleanupEntitiesSystem : SystemBase
+{
+    private EndSimulationEntityCommandBufferSystem ecb;
+
+    protected override void OnCreate()
+    {
+        base.OnCreate();
+
+        ecb = World.GetOrCreateSystem<EndSimulationEntityCommandBufferSystem>();
+    }
+
+    protected override void OnUpdate()
+    {
+        var concurrent = ecb.CreateCommandBuffer().ToConcurrent();
+
+        Entities.ForEach((Entity entity, int entityInQueryIndex, ref LifetimeComponent lifetime) =>
+        {
+            if (lifetime.TimeRemaining == 0f)
+            {
+                concurrent.DestroyEntity(entityInQueryIndex, entity);
+            }
+        }).Schedule();
+
+        ecb.AddJobHandleForProducer(Dependency);
+    }
+}
