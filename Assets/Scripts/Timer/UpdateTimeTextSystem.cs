811c8e166eb4717cdd0ea11e8a4fa770ff69cd1b
Guido Arkesteijn <guidoarkesteijn@gmail.com>
2021-01-07 23:06:55 +0100

    Made system that shows the time with a TextMeshPro component

---
+++ Assets/Scripts/Timer/UpdateTimeTextSystem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using TMPro;
+using Unity.Entities;
+
+public class UpdateTimeTextSystem : SystemBase
+{
+    protected override void OnUpdate()
+    {
+        Entities
+            .WithoutBurst()
+            .ForEach((in TextMeshProUGUI text, in TimeComponent time) =>
+        {
+            text.text = TimeSpan.FromSeconds(time.Time).ToString(@"mm\:ss\:fff");
+        }).Run();
+    }
+}
